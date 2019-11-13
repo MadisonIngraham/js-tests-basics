74c035de3ad4a6d5db52e77e972e1101adea244c
MadisonIngraham <madi.ingraham@gmail.com>
2019-11-12 17:02:47 -0700

    work from 11.12

--- app/numbers.js
+++ app/numbers.js
@@ -1,27 +1,33 @@
 // Write a function that uses the addition operator to add two numbers together
 function add(x, y) {
-
+  let answer = x + y;
+  return answer;
 }
 
 // Write a function that returns the Cubed value of x
 function cubed(x) {
-
+  let answer = Math.pow(x, 3);
+  return answer;
 }
 
 // Write a function that will divide two numbers, and will format the returned value
 // as a whole number. (No Decimals)
 function divideWholey(x, y) {
-
+  let answer = x / y;
+  return parseInt(answer);
 }
 
-// Write a function that takes in a number and determines if it is even. 
+// Write a function that takes in a number and determines if it is even.
 // It should return a boolean value - true if it is even, false if it is odd.
 function isEven(num) {
+  let answer = num % 2;
+  return Boolean(answer == 0);
 }
 
-
-// Given 2 numbers, generate a random number within that range. 
-// Make it inclusive so that the starting and ending numbers are part 
+// Given 2 numbers, generate a random number within that range.
+// Make it inclusive so that the starting and ending numbers are part
 // of the range.HINT ** Utilize Javascript's Math Class (To the Google!)
 function numberGenerator(num1, num2) {
+  let answer = Math.floor(Math.random() * (num2 - num1 + 1)) + num1;
+  return answer;
 }
--- app/strings.js
+++ app/strings.js
@@ -1,12 +1,17 @@
 // Write a function that returns the character from a string at a given position
 function letterAtPosition(str, position) {
-
+  let charPosition = str.charAt(position);
+  return charPosition;
 }
 
-// Write a function that will join two strings with an optional seperating character 
+// Write a function that will join two strings with an optional seperating character
 function concat(str1, str2, seperator = " ") {
+  let product = str1 + seperator + str2;
+  return product;
 }
 
 // Write a function that will capitalize the letter at a given position and return it
 function capitalizer(str, position) {
+  let product = str.charAt(position).toUpperCase();
+  return product;
 }
